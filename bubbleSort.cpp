3b0254ff3655219db5b5dbfd36ef7059b173abdb
Suyash Mudiya <54996412+suyashartworks@users.noreply.github.com>
2021-05-22 15:02:22 +0530

    Add files via upload

---
+++ bubbleSort.cpp
@@ -0,0 +1,52 @@
+// Code by Suyash Mudiya
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Declaring all the variables
+    int numberOfElements, elements[1000], swappingVariable;
+
+    // Taking input
+    cout << "Bubble Sort\n";
+    cout << "Enter number of elements in your array of elements : ";
+    cin >> numberOfElements;
+
+    cout << "Now enter the elements : ";
+    for (int i = 0; i < numberOfElements; i++)
+    {
+        cin >> elements[i];
+    }
+
+    // printing array of elements before sorting
+    cout << "Input array : \n";
+    for (int i = 0; i < numberOfElements; i++)
+    {
+        cout << elements[i] << " ";
+    }
+    cout << endl;
+
+    // Bubble sorting all the elements in different passes
+    for (int i = 0; i < numberOfElements; i++)
+    {
+        for (int j = 0; j < numberOfElements - 1; j++)
+        {
+            if (elements[j] > elements[j + 1])
+            {
+                swappingVariable = elements[j];
+                elements[j] = elements[j + 1];
+                elements[j + 1] = swappingVariable;
+            }
+        }
+    }
+
+    // Printing sorted array of elements
+    cout << "Sorted array : \n";
+    for (int i = 0; i < numberOfElements; i++)
+    {
+        cout << elements[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
